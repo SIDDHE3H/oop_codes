732e5d1837bbd7b471ea00759e37def79f5e5a48
Your Name <your@email.com>
2026-09-12 19:10:52 +0530

    Add loops program

---
+++ EXP3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks[5] = {78, 82, 91, 67, 88};
+    for (int i = 0; i < 5; i++) {
+        cout << marks[i] << " ";
+    }
+    cout << endl; // adds a newline after printing
+    return 0;
+}
